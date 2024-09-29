36f39fcaeca91d39af01c3a77c8e8cb3ce8c22e4
Sayan Das <sayandasraja56@gmail.com>
2024-09-30 01:11:22 +0530

    db connection

--- elib/src/config/config.ts
+++ elib/src/config/config.ts
@@ -3,13 +3,7 @@ conf();
 
 const _config = {
   port: process.env.PORT,
-  databaseUrl: process.env.MONGO_CONNECTION_STRING,
-  env: process.env.NODE_ENV,
-  jwtSecret: process.env.JWT_SECRET,
-  cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
-  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
-  cloudinarySecret: process.env.CLOUDINARY_API_SECRET,
-  frontendDomain: process.env.FRONTEND_DOMAIN,
+  databaseUrl: process.env.MONGODB_URI,
 };
 
 export const config = Object.freeze(_config);
